e24fcdb5c94dbf450a41ee16311aa576181f8c04
MandiaRoux <mandiaroux@gmail.com>
2023-08-04 17:25:40 +0200

    Init rollup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-helpers
 Stuff that is helpful
+
+https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
---
+++ src/components/SwipeableDrawer/SwipeableDrawer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface SwipeableDrawerProps {}
+
+const SwipeableDrawer = (props: SwipeableDrawerProps) => {
+  return <div>hi</div>;
+};
+
+export default SwipeableDrawer;
